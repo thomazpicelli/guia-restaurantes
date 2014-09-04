d4c070ff2ca63b314ac7c022dd55da6d0b67ea74
31452760 <31452760@087999-P33S105.macklabs.br>
2014-09-04 11:59:06 -0300

    5.2 ENTREGA

--- operacoes.php
+++ operacoes.php
@@ -21,33 +21,34 @@
                 return $z;
             }
             function divisão ($x, $y){
-                $z = $x / $y;
-                return $z;
+                if ($y == 0 ){
+                    echo "Não existe divisão por zero!";
+                }
+                else{    
+                    $z = $x / $y;
+                    return $z;
+                }    
             }
             
             $n1 = 10;
             $n2 = 20;
     
             $resultado = soma($n1, $n2);
-
             imprime_resultado($resultado);
             
             echo "<br><br>";
             
             $resultado = subtração($n1, $n2);
-
             imprime_resultado($resultado);
             
             echo "<br><br>";
             
             $resultado = multiplicação($n1, $n2);
-
             imprime_resultado($resultado);
             
             echo "<br><br>";
             
             $resultado = divisão($n1, $n2);
-
             imprime_resultado($resultado);
             
             
