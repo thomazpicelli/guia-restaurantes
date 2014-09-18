f1fba9d99e2fd8b2910cf3e2ce4fb9f25ab0c534
31452760 <31452760@087999-P33S105.macklabs.br>
2014-09-18 11:20:54 -0300

    Dvisão entre funções e execução.

--- operacoes.php
+++ operacoes.php
@@ -29,31 +29,6 @@
                     return $z;
                 }    
             }
-            
-            $n1 = 10;
-            $n2 = 20;
-    
-            $resultado = soma($n1, $n2);
-            imprime_resultado($resultado);
-            
-            echo "<br><br>";
-            
-            $resultado = subtração($n1, $n2);
-            imprime_resultado($resultado);
-            
-            echo "<br><br>";
-            
-            $resultado = multiplicação($n1, $n2);
-            imprime_resultado($resultado);
-            
-            echo "<br><br>";
-            
-            $resultado = divisão($n1, $n2);
-            imprime_resultado($resultado);
-            
-            
-            echo "<br><br>";
-
                ?>
     </body>
 </html>
--- teste.php
+++ teste.php
@@ -3,7 +3,30 @@
     <head></head>
     <body>
         <?php include "operacoes.php"; 
+                        $n1 = 10;
+            $n2 = 20;
+    
+            $resultado = soma($n1, $n2);
+            imprime_resultado($resultado);
             
+            echo "<br><br>";
+            
+            $resultado = subtração($n1, $n2);
+            imprime_resultado($resultado);
+            
+            echo "<br><br>";
+            
+            $resultado = multiplicação($n1, $n2);
+            imprime_resultado($resultado);
+            
+            echo "<br><br>";
+            
+            $resultado = divisão($n1, $n2);
+            imprime_resultado($resultado);
+            
+            
+            echo "<br><br>";
+
         ?>
     </body>
 </html>
